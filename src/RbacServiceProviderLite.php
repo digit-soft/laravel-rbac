0fbfb975864611d3d726547a642ed7577263c6b4
digit <digit.vova@gmail.com>
2018-08-02 12:26:53 +0300

    Add one more service provider

---
+++ src/RbacServiceProviderLite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DigitSoft\LaravelRbac;
+
+use Illuminate\Support\ServiceProvider;
+
+class RbacServiceProviderLite extends ServiceProvider
+{
+    protected $defer = false;
+
+    public function register()
+    {
+
+    }
+
+}
