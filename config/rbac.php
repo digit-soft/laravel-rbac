f167d2b1875b405694de2f03863d40026be81b7a
digit <digit.vova@gmail.com>
2018-08-03 18:03:06 +0300

    basic config update

--- config/rbac.php
+++ config/rbac.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'itemSource' => '\\DigitSoft\\LaravelRbac\\Sources\\PhpFileSource',
+    'admin_roles' => ['Admin'],
+    //'storage' => 'DigitSoft\LaravelRbac\Storages\PhpFileStorage',
+    'storage' => 'DigitSoft\LaravelRbac\Storages\DbStorage',
+    'checker' => 'DigitSoft\LaravelRbac\Checkers\Basic',
+    'item_file' => 'rbac/items.php',
+    'assigns_file' => 'rbac/assigns.php',
+    'users_table' => 'users',
+    'cache_enable' => false,
+    'cache_duration' => 1 //in minutes
 ];
