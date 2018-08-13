bd92c57b8c6238ae005602a4abc89afdfa80c0b2
digit <digit.vova@gmail.com>
2018-08-13 14:38:03 +0300

    Add PermissionCheck middleware

---
+++ src/Middleware/PermissionCheck.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DigitSoft\LaravelRbac\Middleware;
+
+use DigitSoft\LaravelRbac\Facades\Rbac;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Class PermissionCheck.
+ * Middleware for permissions|roles check
+ * @package DigitSoft\LaravelRbac\Middleware
+ */
+class PermissionCheck
+{
+    /**
+     * Handle request
+     * Check permissions or roles. Permissions can be passed as `|` separated array
+     * @param Request  $request
+     * @param \Closure $next
+     * @param string|array $permission
+     * @return mixed
+     */
+    public function handle(Request $request, \Closure $next, $permission)
+    {
+        if (auth()->guest()) {
+            throw new AccessDeniedHttpException();
+        }
+        $permissions = is_array($permission)
+            ? $permission
+            : explode('|', $permission);
+        if (!Rbac::has($permissions)) {
+            throw new AccessDeniedHttpException();
+        }
+        return $next($request);
+    }
+}
